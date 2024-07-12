3be10c027a2731cf5b3ecfb06d3fe03930be1d46
Skand Bajpai <99634361+Skumm00@users.noreply.github.com>
2024-07-12 21:37:40 +0000

    Added a simple calculator, rebuilt my foundations on the basic syntax and operators

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def testfunction():
   print(x,y,z + " are Tom's Friends!", rc )
 
 testfunction()
-print(x,y,z + "Are toms friends!", rc )"""
+print(x,y,z + "Are toms friends!", rc )
 
 #Common Name Generator
 #with only 4 premade names
@@ -41,3 +41,33 @@ a = b = c = d = names
 random_num = input("Enter a Number to get your premade name(0-4):")
 x = int(random_num)
 print(names[0+x])
+#simple basic stuff esentially
+
+def main():
+  print("This is text coming from the main function of the program!")
+  secondary_function()
+
+def secondary_function():
+  print("This is text coming from the secondary function of the program!")
+
+if __name__ == '__main__':
+  main()
+  """
+#Python Operators/Calculator
+basic = float(input("Enter a number:"))
+basic2 = float(input("Enter 2nd number:"))
+logic = input("Add Operator(+,-,*,/):")
+if logic == "+":
+  print(basic + basic2)
+elif logic == "-":
+  print(basic - basic2)
+elif logic == "*":
+  print(basic * basic2)
+elif logic == "/": 
+  print(basic/basic2)
+elif basic2 == 0 and basic == 0 and logic == "/":
+  print("Error. Please Restart Program FOOL")
+else:
+  print("Please put in a valid operator! (+,-,*,/)")
+  logic = input("Add Operator(+,-,*,/):")
+print(logic)
