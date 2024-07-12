a2467e3ac4036ad019691043bc9a1117dd7cfe2e
Skand Bajpai <99634361+Skumm00@users.noreply.github.com>
2024-07-12 19:50:59 +0000

    Finished relearning(more like recoding) Variables, Data Types, Syntax, and comments

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World!")
+x = 5
+y = "Hello World CHAT"
+
+if x > 2:
+  print(y)
+
+#Beginner Stuff doing again- Refreshing my MEMORY
+
+Name = "John"
+
+Woman_Name = "Mary"
+
+print(Name + " and " + Woman_Name + " are Sisters")
+
+x = str(3)
+
+x = int(4)
+
+x = float(3)
+
+print(type(x))
+
+names = ["John", "Rajesh", "Lina"]
+x,y,z = names
+
+print(x,y,z)
